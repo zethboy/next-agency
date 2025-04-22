3979fc8f91679bc82be47e20e006dafccd36c5b6
zethboy <yoginugraha842@gmail.com>
2025-04-22 19:47:05 +0700

    added file

---
+++ public/assets/data.js
@@ -0,0 +1,17 @@
+import Hero from "@/public/assets/company.webp";
+import Proyek1 from "@/public/assets/proyek1.webp";
+import Proyek2 from "@/public/assets/proyek2.webp";
+import Proyek3 from "@/public/assets/proyek3.webp";
+import Proyek4 from "@/public/assets/proyek4.webp";
+import Proyek5 from "@/public/assets/proyek5.webp";
+
+const Image = {
+  Hero,
+  Proyek1,
+  Proyek2,
+  Proyek3,
+  Proyek4,
+  Proyek5,
+};
+
+export default Image;
