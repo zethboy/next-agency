d5f4aeedab3b9b2948685f9bf8c9c6aea805578b
zethboy <yoginugraha842@gmail.com>
2025-04-23 12:49:39 +0700

    update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -20,7 +20,7 @@ const handleClick = () => {
           <div className="logo">
             <h1 className='text-3xl font-bold'>Ngoding</h1>
           </div>
-          <ul className={`menu flex items-center gap-12 md:static absolute left-1/2 -translate-x-1/2 ${active ? "top-24 opacity-100" : "top-20 opacity-0"} md:-translate-x-0 md:flex-row flex-col md:bg-transparent bg-slate-700 w-full md:w-auto md:py-0 py-10 text-white md:text-black transition-all md:opacity-100`}>
+          <ul className={`menu flex items-center gap-12 md:static absolute left-1/2 -translate-x-1/2 ${active ? "top-24 opacity-100" : "top-20 opacity-0"} md:-translate-x-0 md:flex-row flex-col md:bg-transparent bg-slate-700 w-full md:w-auto md:py-0 py-10 text-white md:text-black transition-all md:opacity-100 md:transition-none`}>
             <li>
               <Link href={"#beranda"}>Beranda</Link>
             </li>
