2f1906797ff49a513d73f839eb627e92e1b563b4
zethboy <yoginugraha842@gmail.com>
2025-06-13 19:52:33 +0700

    update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -28,7 +28,7 @@ const handleClick = () => {
               <Link href={"#layanan"}>Service</Link>
             </li>
             <li>
-              <Link href={"#proyek"}>Tentang Kami</Link>
+              <Link href={"#proyek"}>Tentang</Link>
             </li>
             <li>
               <Link href={"#kontak"}>Kontak</Link>
