63321e8c4c53799a63fefa4009ce2a002ffceff4
zethboy <yoginugraha842@gmail.com>
2025-06-21 20:55:04 +0700

    aboutUs section

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Hero from '../components/Hero';
+import AboutUs from '../components/AboutUs';
 
 export default function Home() {
   return (
    <div>
     <Hero />
+    <AboutUs />
     {/* <h1>Homepage</h1> */}
    </div>
   );
